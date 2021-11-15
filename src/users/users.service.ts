27e50ae556b2bc44994f6fede195078ec38468ff
Mohan Murali <mohanmuralid28@gmail.com>
2021-11-15 22:29:15 +0530

    added custom interceptor

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+
+    return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    return this.repo.findOne(id);
+  }
+
+  find(email: string) {
+    return this.repo.find({ email });
+  }
+
+  async update(id: number, attrbs: Partial<User>) {
+    const user = await this.repo.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    Object.assign(user, attrbs);
+
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.repo.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    return this.repo.remove(user);
+  }
+}
